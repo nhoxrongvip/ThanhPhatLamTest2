f959821d381f06fe7e53ff589ccf655968ca5580
nhoxrongvip <phatlam.media.vis@gmail.com>
2021-03-04 18:10:01 -0500

    Initial commit

---
+++ LamTestCD/src/main/java/thanh/lam/n01335598/LamActivity2.java
@@ -0,0 +1,15 @@
+//Thanh Phat Lam N01335598 CENG258RNC
+package thanh.lam.n01335598;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LamActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lam2);
+    }
+}
